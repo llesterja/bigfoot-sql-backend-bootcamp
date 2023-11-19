5a828ab01fea1eb69545471530ac4693685b241d
llesterja <lesterlim.ja@gmail.com>
2023-11-19 18:43:17 +0800

    added post and delete methods to the controller

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -15,6 +15,35 @@ class SightingsController extends BaseController {
       return res.status(400).json({ error: true, msg: err });
     }
   }
+
+  async addNew(req,res){
+    const {date,location,notes}=req.body;
+    try {
+      const newSighting = await this.model.create({
+        date:new Date(date),
+        location:location,
+        notes:notes
+      });
+      return res.json(newSighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+  async removeOne(req,res){
+    const { sightingId } = req.params;
+    try {
+      const removeSighting = await this.model.destroy({
+        where:{
+          id: sightingId
+        }
+      });
+      return res.json(removeSighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  
 }
 
 module.exports = SightingsController;
--- routers/sightingsRouter.js
+++ routers/sightingsRouter.js
@@ -9,6 +9,8 @@ class SightingsRouter {
     // we will insert routes into here later on
     router.get("/", this.controller.getAll.bind(this.controller));
     router.get("/:sightingId", this.controller.getOne.bind(this.controller));
+    router.delete("/:sightingId", this.controller.removeOne.bind(this.controller));
+    router.post("/",this.controller.addNew.bind(this.controller));
     return router;
   }
 }
